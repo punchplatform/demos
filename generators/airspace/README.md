ae28ba300f1ca0a69e1b5e89955b08b0160d7de7
pierre.dosne <pierre.dosne@thalesgroup.com>
2020-11-19 15:33:11 +0100

    delete unused resources

--- generators/airspace/README.md
+++
@@ -1,5 +0,0 @@
-# python_project_starter_kit
-This is a python project starter kit
-
-
-> Pierre Dosne
--- generators/airspace/project/test/moduleTest/module_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-
-class ModuleTest(unittest.TestCase):
-    def setUp(self) -> None:
-        self.message = "This is a test"
-
-    def testMessage(self):
-        self.assertEqual(self.message, "This is a test")
