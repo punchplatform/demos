80f655431bbba80411495ba3a01a6572d493360e
pierre.dosne <pierre.dosne@thalesgroup.com>
2020-11-27 10:49:48 +0100

    update airspace uc

--- generators/tool/setup.py
+++ generators/tool/setup.py
@@ -7,7 +7,8 @@ setuptools.setup(
     install_requires=[
     "numpy==1.19.4",
     "mlflow==1.12.1",
-    "scikit-learn==0.23.2"
+    "scikit-learn==0.23.2",
+    "elasticsearch==7.10.0"
     ],    
     packages=setuptools.find_packages(),
 )
