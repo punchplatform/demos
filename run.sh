52fe2726230ca2519e195e03e76fa3c7aa7eaf40
pierre.dosne <pierre.dosne@thalesgroup.com>
2021-02-02 11:54:31 +0100

    update script documentation

--- run.sh
+++ run.sh
@@ -31,28 +31,29 @@ echo "COPY KIBANA RESOURCES"
 cp -r $PUNCH_DEMO_DIR/conf/resources/kibana/dashboards/* $PUNCHPLATFORM_CONF_DIR/resources/kibana/dashboards/
 echo "COPY ES RESOURCES"
 cp -r $PUNCH_DEMO_DIR/conf/resources/elasticsearch/* $PUNCHPLATFORM_CONF_DIR/resources/elasticsearch/
-echo "COPY SPARK RESOURCES"
+echo "COPY PYSPARK NODES"
 cp -r $PUNCH_DEMO_DIR/conf/resources/spark_custom_nodes/pyspark $PUNCHPLATFORM_CONF_DIR/resources/spark_custom_nodes/pyspark
-echo "Import all Dashboards"
-punchplatform-setup-kibana.sh --import
-echo "Import ES templates"
-punchplatform-push-es-templates.sh --directory $PUNCHPLATFORM_CONF_DIR/resources/elasticsearch/
-echo "Build generators code"
-make package -C $PUNCH_DEMO_DIR/generators/tool name=tool
-echo "CREATE DATASETS"
-if [ ! -d /data ]
-then
-	mkdir /data
-fi
-python $PUNCH_DEMO_DIR/generators/tool/dist/tool.pex -c $PUNCH_DEMO_DIR/generators/tool/config/config.json
-echo "Build all pyspark nodes"
+echo "BUILD ALL PYSPARK NODES"
 make -C $PUNCHPLATFORM_CONF_DIR/resources/spark_custom_nodes/pyspark all
-echo "Copy pyspark nodes to your $PUNCHPLATFORM_BINARIES_DIR/extlib"
+echo "COPY PYSPARK NODES TO YOUR $PUNCHPLATFORM_BINARIES_DIR/extlib"
 if [ ! -d $PUNCHPLATFORM_BINARIES_DIR/extlib/pyspark ]
 then
 	mkdir -p $PUNCHPLATFORM_BINARIES_DIR/extlib/pyspark
 fi
 cp $PUNCHPLATFORM_CONF_DIR/resources/spark_custom_nodes/pyspark/target/* $PUNCHPLATFORM_BINARIES_DIR/extlib/pyspark
-echo "PUSH SOME DATA INTO ES"
+echo "IMPORT ALL DASHBOARDS"
+punchplatform-setup-kibana.sh --import
+echo "IMPORT ES TEMPLATES"
+punchplatform-push-es-templates.sh --directory $PUNCHPLATFORM_CONF_DIR/resources/elasticsearch/
+echo "BUILD PYTHON GENERATORS PROJECT"
+make package -C $PUNCH_DEMO_DIR/generators/tool name=tool
+echo "CREATE /data REPO"
+if [ ! -d /data ]
+then
+	mkdir /data
+fi
+echo "CREATE DATASETS"
+python $PUNCH_DEMO_DIR/generators/tool/dist/tool.pex -c $PUNCH_DEMO_DIR/generators/tool/config/config.json
+echo "PUSH SOME MANDATORY DATA INTO ES"
 curl -H "Content-type: application/json" --data-binary @$PUNCH_DEMO_DIR/data/world_map.json -XPOST http://localhost:9200/world-map-import/_bulk?pretty
 echo "...Installation completed"
