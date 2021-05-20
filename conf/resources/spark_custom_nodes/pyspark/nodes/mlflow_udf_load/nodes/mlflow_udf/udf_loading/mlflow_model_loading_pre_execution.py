9f8d91ca451cf3c166d83757c0d7565c2860f4f6
pierre.dosne <pierre.dosne@thalesgroup.com>
2021-05-20 16:30:59 +0200

    Add registry call to mlflow_udf

--- conf/resources/spark_custom_nodes/pyspark/nodes/mlflow_udf_load/nodes/mlflow_udf/udf_loading/mlflow_model_loading_pre_execution.py
+++ conf/resources/spark_custom_nodes/pyspark/nodes/mlflow_udf_load/nodes/mlflow_udf/udf_loading/mlflow_model_loading_pre_execution.py
@@ -18,7 +18,9 @@ from pyspark.sql.types import StringType
 from pyspark.sql.types import DoubleType
 from pyspark.sql.session import SparkSession
 from pyspark import SparkConf
-import mlflow.pyfunc
+import mlflow
+import requests
+import json
 
 __author__ = "pierre"
 
@@ -32,13 +34,30 @@ class MlflowModelLoadingPreExecution(object):
 
     def pre(self) -> None:
         data_type = SparkConf().get("spark.punch.mlflow.model.type")
-        model_uri = SparkConf().get("spark.punch.mlflow.model.uri")
+        model_registry_id = SparkConf().get("spark.punch.mlflow.model.registry.id")
+        model_registry_stage = SparkConf().get("spark.punch.mlflow.model.registry.stage")
+        mlflow_host = SparkConf().get("spark.punch.mlflow.host")
+        mlflow_port = SparkConf().get("spark.punch.mlflow.port")
         
+        # Test output expected type
         if(data_type == 'regression'):
             data_type = DoubleType()
         
         elif( (data_type == 'classification') or (data_type == 'clustering') ):
             data_type = StringType()
 
-        prediction = mlflow.pyfunc.spark_udf(spark=self.__spark_session, model_uri=model_uri, result_type=data_type)
+        # Fetch the latest model version
+        registry_uri = "http://{hostname}:{port}/api/2.0/preview/mlflow/registered-models/get".format(hostname=mlflow_host, port=mlflow_port)
+        registry_params = {"name" : model_registry_id}
+        result = requests.get(registry_uri, registry_params)
+        payload = json.loads(result.text)
+        model_latest = payload["registered_model"]["latest_versions"]
+        model_to_return = None
+        for model in model_latest:
+            if(model['current_stage'].lower() == current_model_registry_stage.lower()):
+                model_to_return = model
+            else:
+                pass
+        # Generate a pandas UDF function
+        prediction = mlflow.pyfunc.spark_udf(spark=self.__spark_session, model_uri=model_to_return, result_type=data_type)
         self.__spark_session.udf.register("prediction", prediction)
