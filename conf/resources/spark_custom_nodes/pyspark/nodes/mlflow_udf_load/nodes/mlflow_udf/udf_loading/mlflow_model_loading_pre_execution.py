3989fd6e0896be03fdd4ed6248800c060f719637
pierre.dosne <pierre.dosne@thalesgroup.com>
2021-05-21 14:32:00 +0200

    update latest model version fetch

--- conf/resources/spark_custom_nodes/pyspark/nodes/mlflow_udf_load/nodes/mlflow_udf/udf_loading/mlflow_model_loading_pre_execution.py
+++ conf/resources/spark_custom_nodes/pyspark/nodes/mlflow_udf_load/nodes/mlflow_udf/udf_loading/mlflow_model_loading_pre_execution.py
@@ -38,26 +38,16 @@ class MlflowModelLoadingPreExecution(object):
         model_registry_stage = SparkConf().get("spark.punch.mlflow.model.registry.stage")
         mlflow_host = SparkConf().get("spark.punch.mlflow.host")
         mlflow_port = SparkConf().get("spark.punch.mlflow.port")
-        
+        model_uri = "models:/{model}/{state}".format(model=model_registry_id, state=cmodel_registry_stage)
         # Test output expected type
         if(data_type == 'regression'):
             data_type = DoubleType()
         
         elif( (data_type == 'classification') or (data_type == 'clustering') ):
             data_type = StringType()
+        
+        mlflow.set_tracking_uri("http://{hostname}:{port}".format(hostname=mlflow_host, port=mlflow_port))
 
-        # Fetch the latest model version
-        registry_uri = "http://{hostname}:{port}/api/2.0/preview/mlflow/registered-models/get".format(hostname=mlflow_host, port=mlflow_port)
-        registry_params = {"name" : model_registry_id}
-        result = requests.get(registry_uri, registry_params)
-        payload = json.loads(result.text)
-        model_latest = payload["registered_model"]["latest_versions"]
-        model_to_return = None
-        for model in model_latest:
-            if(model['current_stage'].lower() == current_model_registry_stage.lower()):
-                model_to_return = model
-            else:
-                pass
         # Generate a pandas UDF function
-        prediction = mlflow.pyfunc.spark_udf(spark=self.__spark_session, model_uri=model_to_return, result_type=data_type)
+        prediction = mlflow.pyfunc.spark_udf(spark=self.__spark_session, model_uri=model_uri, result_type=data_type)
         self.__spark_session.udf.register("prediction", prediction)
